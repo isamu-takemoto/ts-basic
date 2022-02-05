e55121bd2ceda8e67386ed6dbdf3b2af3968f840
isamu-takemoto <takeisa0201@gmail.com>
2022-02-05 22:19:55 +0900

    add config

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-## init
+# xxx
+# ts-basic
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import World from './world'
+const root: HTMLElement | null = document.getElementById('root')
+const world = new World('Hello World!!!')
+world.sayHello(root)
